1fe7103132e2f8525b2eed70ce5ff95f3dffd05d
Amirali Rafee <amirali2439amir@gmail.com>
2025-10-27 18:09:24 +0330

    Fix: Use env vars instead of .pem files

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ console.log('12. ایمیل تنظیم شد');
 console.log('13. در حال لود کلیدها از فایل...');
 let privateKey, publicKey;
 try {
-  privateKey = fs.readFileSync(path.join(__dirname, 'private.pem'), 'utf8');
-  publicKey = fs.readFileSync(path.join(__dirname, 'public.pem'), 'utf8');
+  const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+  const publicKey = process.env.PUBLIC_KEY.replace(/\\n/g, '\n');
   console.log('14. کلیدها با موفقیت لود شدند');
 } catch (err) {
   console.error('خطا در لود کلیدها از فایل:', err.message);
